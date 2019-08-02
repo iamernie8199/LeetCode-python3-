43f09c9f1cb37fad2ce5eafc69b9dc151192ffb1
iamernie8199 <iamernie8199@gmail.com>
2019-08-02 17:31:31 +0800

    Create R to Int.py

---
+++ 13. Roman to Integer/R to Int.py
@@ -0,0 +1,46 @@
+"""
+Given a roman numeral, convert it to an integer. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+Input: "III"
+Output: 3
+
+Example 2:
+Input: "IV"
+Output: 4
+
+Example 3:
+Input: "IX"
+Output: 9
+
+Example 4:
+Input: "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 5:
+Input: "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+"""
+
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dictr = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        ans = 0
+        for i in range(len(s)):
+            ans += dictr[s[i]]
+            if i == 0:
+                continue
+            elif dictr[s[i]] > dictr[s[i-1]]:
+                ans -= dictr[s[i-1]]*2
+        return ans
