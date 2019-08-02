c81f5e57cc123f7400686fd53849ef8c29edaaad
iamernie8199 <iamernie8199@gmail.com>
2019-08-02 17:53:55 +0800

    Create int to R.py

---
+++ 12. Integer to Roman/int to R.py
@@ -0,0 +1,42 @@
+"""
+Given an integer, convert it to a roman numeral. Input is guaranteed to be within the range from 1 to 3999.
+
+Example 1:
+Input: 3
+Output: "III"
+
+Example 2:
+Input: 4
+Output: "IV"
+
+Example 3:
+Input: 9
+Output: "IX"
+
+Example 4:
+Input: 58
+Output: "LVIII"
+Explanation: L = 50, V = 5, III = 3.
+
+Example 5:
+Input: 1994
+Output: "MCMXCIV"
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+"""
+
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        ones = {0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX'}
+        tens = {0: '', 1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L', 6: 'LX', 7: 'LXX', 8: 'LXXX', 9: 'XC'}
+        hundreds = {0: '', 1: 'C', 2: 'CC', 3: 'CCC', 4: 'CD', 5: 'D', 6: 'DC', 7: 'DCC', 8: 'DCCC', 9: 'CM'}
+        thousands = {0: '', 1: 'M', 2: 'MM', 3: 'MMM'}
+
+        i = 0
+        r = []
+        while i < 4:
+            r.append(num % 10)
+            num //= 10
+            i += 1
+        Roman = thousands[r[3]] + hundreds[r[2]] + tens[r[1]] + ones[r[0]]
+        return Roman
