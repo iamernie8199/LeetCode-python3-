e637a7e498132ddc7d82927c4200569f9a9be052
iamernie8199 <iamernie8199@gmail.com>
2019-08-02 19:20:26 +0800

    Create Jewels.py

---
+++ 771. Jewels and Stones/Jewels.py
@@ -0,0 +1,16 @@
+"""
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+
+Example 2:
+Input: J = "z", S = "ZZ"
+Output: 0
+"""
+
+
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        return sum(s in J for s in S)
